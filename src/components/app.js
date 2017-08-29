cd3b7f95101d6c896d7d6aeb5d2e4f4ec7777d82
Sean Wines <sean.wines@vuria.com>
2017-08-29 11:58:47 -0700

    Created add todo logic

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import Input from './input';
 
-export default class App extends Component {
-  render() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: []
+    }
+
+    const addTodo = this.addTodo.bind(this);
+  }
+
+  addTodo(item) {
+    this.setState({
+      todos: this.state.todos.concat(item)
+    })
+  }
+
+  renderTodoList() {
     return (
-      <div>React simple starter</div>
-    );
+      <ul>
+        {this.state.todos.map( item => {
+          return <li>{ item }</li>
+        })}
+      </ul>
+    )
+  }
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="container">
+        <Input addTodo={ this.addTodo.bind(this) } />
+          { this.renderTodoList() }
+      </div>
+    )
   }
 }
+
+export default App;
---
+++ src/components/input.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: ''
+    }
+  }
+
+  onInputChange(item) {
+    this.setState({ item })
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={ this.state.item }
+          onChange={ e => this.onInputChange(e.target.value) } />
+        <button onClick={ () => this.props.addTodo(this.state.item) }>+</button>
+      </div>
+    )
+  }
+}
+
+export default Input;
